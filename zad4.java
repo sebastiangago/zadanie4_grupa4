e50c25e12140436f0eb6f8074d649e7e8cb735d6
sebastiangago <sebastiangago02@gmail.com>
2024-10-22 19:59:44 +0200

    added zad4.java

---
+++ zad4.java
@@ -0,0 +1,6 @@
+public class zad4 {
+        public static void main(String[] args) {
+            System.out.println("Hello Vistula");
+            System.out.println("Hello World");
+        }
+    }
